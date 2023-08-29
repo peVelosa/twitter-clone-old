2a8a9028657069a2a13c17e8f4e6f8068145c91d
Pedro Velosa <90364573+peVelosa@users.noreply.github.com>
2023-08-28 21:03:37 -0300

    Delete route.ts

--- src/app/api/route.ts
+++
@@ -1,8 +0,0 @@
-import prisma from "@/libs/prisma";
-import { NextResponse } from "next/server";
-
-export async function GET() {
-  const res = await prisma.user.findMany({});
-
-  return NextResponse.json({ data: res });
-}
