f512f9d0c8f50264b4c4a7b4d1f5153076b22a11
Pedro Enrique Pereira Velosa <m_pedro.velosa@fiap.com.br>
2023-09-01 16:12:14 -0300

    type for tweet page

--- src/app/(app)/tweet/[id]/page.tsx
+++ src/app/(app)/tweet/[id]/page.tsx
@@ -1,4 +1,10 @@
-const ServerTweetPage = ({ params: { id } }) => {
+import type { FC } from "react";
+
+type ServerTweetPageProps = {
+  params: { id: string };
+};
+
+const ServerTweetPage: FC<ServerTweetPageProps> = ({ params: { id } }) => {
   return <div>oioi {id}</div>;
 };
 
